c39f7528b4cf69d4bd63ce0fa2ab0918609c85ad
samad <samadarshad@hotmail.co.uk>
2021-02-09 17:11:32 +0000

    Importing stuff

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux'
+import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider as StoreProvider } from 'react-redux';
+import middleware from './middleware'
+import reducer from './reducers'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+
+  render() {
+    const store = createStore(reducer, middleware)
+
+    return (
+      <StoreProvider store={store}>
+        <PaperProvider>
+          <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+          </View>
+        </PaperProvider>
+      </StoreProvider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ reducers/index.js
@@ -0,0 +1,5 @@
+function decks(state = {}, action) {
+    return state
+}
+
+export default decks
