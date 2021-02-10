657b7b3ade9edaefc96cb6730bf35aeee60f40a0
samad <samadarshad@hotmail.co.uk>
2021-02-10 17:52:23 +0000

    add deck

--- components/AddDeckView.js
+++ components/AddDeckView.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
-import { List, Card, Title, Paragraph } from 'react-native-paper';
+import { List, Card, Title, Paragraph, Button, TextInput } from 'react-native-paper';
 
 class AddDeckView extends Component {
+    state = {
+        text: ''
+    }
     render() {
         return (
-            <View style={styles.container}>
-                <Text>AddDeckView</Text>
-
+            <View style={{
+                flex: 1, marginHorizontal: 50, justifyContent: 'center'
+            }}>
+                <Title style={{ textAlign: 'center' }}>What is the title of your new deck?</Title>
+                <TextInput
+                    label="Deck Title"
+                    value={this.state.text}
+                    onChangeText={text => this.setState({ text })}
+                />
+                <Button mode="contained" onPress={() => console.log('submit new deck')} style={{ margin: 10 }}>
+                    Submit
+                </Button>
             </View>
         )
     }
