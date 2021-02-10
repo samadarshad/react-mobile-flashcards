e02216698804433a3f1cbd883121ddb25cb3f917
samad <samadarshad@hotmail.co.uk>
2021-02-10 17:58:18 +0000

    add qn

--- components/AddQuestionView.js
+++ components/AddQuestionView.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
+import { List, Card, Title, Paragraph, Button, TextInput } from 'react-native-paper';
 
 class AddQuestionView extends Component {
+    state = {
+        question: '',
+        answer: '',
+    }
     render() {
         return (
-            <View style={styles.container}>
-                <Text>add a question</Text>
+            <View style={{
+                flex: 1, marginHorizontal: 50, justifyContent: 'center'
+            }}>
+                <TextInput
+                    label="Question"
+                    value={this.state.question}
+                    onChangeText={question => this.setState({ question })}
+                    style={{ marginBottom: 10 }}
+                />
+                <TextInput
+                    label="Answer"
+                    value={this.state.answer}
+                    onChangeText={answer => this.setState({ answer })}
+                />
+                <Button mode="contained" onPress={() => console.log('submit new qn')} style={{ margin: 10 }}>
+                    Submit
+                </Button>
             </View>
         )
     }
@@ -28,6 +48,5 @@ const styles = StyleSheet.create({
         fontSize: 32,
     },
 });
-
 
 export default AddQuestionView
